7f3760c2398c11b324760ba0e6b805614ec9dbc0
David Holovii <mirage.present@gmail.com>
2018-06-13 20:59:03 +0300

    Vue with Apollo

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const VUE_APP_API_AUTH = 'https://jcxz7irz4b.execute-api.us-west-2.amazonaws.com/dev/auth'
+export const VUE_APP_API_QL = 'https://jcxz7irz4b.execute-api.us-west-2.amazonaws.com/dev/graphQL'
--- src/main.js
+++ src/main.js
@@ -4,10 +4,35 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+// Vue Apollo
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+import { VUE_APP_API_QL } from './config.js'
+
+const httpLink = new HttpLink({
+  uri: VUE_APP_API_QL
+})
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+Vue.use(VueApollo)
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  provide: apolloProvider.provide(),
   render: h => h(App)
 }).$mount('#app')
